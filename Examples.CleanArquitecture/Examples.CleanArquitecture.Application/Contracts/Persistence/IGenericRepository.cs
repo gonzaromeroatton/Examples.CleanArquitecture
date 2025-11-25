0bc0b0df72aeae8032b41b8c737538a6eb521cc3
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-25 10:52:51 -0300

    Añadido proyecto de persistencia y repositorios
    Se agregó el proyecto `Examples.CleanArquitecture.Persistence` al archivo de solución, configurado para .NET 9.0 con soporte para Entity Framework Core y SQL Server.
    
    Se añadieron las clases `PersonsDatabaseContext` y `PersonTypeConfiguration` para la gestión del contexto de base de datos y la configuración de la entidad `Person`.
    
    Se implementaron un repositorio genérico (`GenericRepository<T>`) y un repositorio específico para `Person` (`PersonRepository`), junto con las interfaces `IGenericRepository<T>` e `IPersonRepository`.
    
    Se añadió la propiedad `DNI` a las clases `PersonDto` y `Person`. También se incluyó la clase `PersistenceServiceRegistration` para registrar servicios de persistencia en el contenedor de dependencias.

---
+++ Examples.CleanArquitecture/Examples.CleanArquitecture.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using Examples.CleanArquitecture.Domain.Common;
+
+namespace Examples.CleanArquitecture.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
---
+++ Examples.CleanArquitecture/Examples.CleanArquitecture.Application/Contracts/Persistence/IPersonRepository.cs
@@ -0,0 +1,8 @@
+﻿using Examples.CleanArquitecture.Domain;
+
+namespace Examples.CleanArquitecture.Application.Contracts.Persistence;
+
+public interface IPersonRepository : IGenericRepository<Person>
+{
+    Task<bool> IsPersonDNIValid(string name);
+}
