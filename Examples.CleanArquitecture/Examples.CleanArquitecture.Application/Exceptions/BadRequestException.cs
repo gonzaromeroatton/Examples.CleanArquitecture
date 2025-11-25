c5f9084b6b97b60910cb8e410053354419ce1016
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-25 10:04:45 -0300

    Añadidas excepciones y referencia a FluentValidation
    Se eliminó la carpeta `Exceptions` del archivo de proyecto
    `Examples.CleanArquitecture.Application.csproj`.
    
    Se agregó la referencia al paquete NuGet `FluentValidation`
    versión `12.1.0` en el archivo de proyecto.
    
    Se creó la clase `BadRequestException` con soporte para errores
    de validación utilizando `ValidationResult`.
    
    Se creó la clase `NotFoundException` para manejar casos donde
    un recurso no es encontrado.

---
+++ Examples.CleanArquitecture/Examples.CleanArquitecture.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.Results;
+
+namespace Examples.CleanArquitecture.Application.Exceptions;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class BadRequestException : Exception
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    public BadRequestException(string message) : base(message) { }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="validationResult"></param>
+    public BadRequestException(string message, ValidationResult validationResult)
+    {
+        ValidationErrors = validationResult.ToDictionary();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public IDictionary<string, string[]> ValidationErrors { get; set; } = new Dictionary<string, string[]>();
+}
