b549cc55ec56a4a884b0da01903e12521ab82a15
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-26 12:33:37 -0300

    Refactorización de handlers y mejora en BadRequestException
    Se refactorizaron las clases `GetPersonByDniQueryHandle` y
    `GetPersonByIdQueryHandle`, renombrándolas a
    `GetPersonByDniQueryHandler` y `GetPersonByIdQueryHandler`,
    respectivamente, siguiendo una convención de nombres más estándar.
    Se mantuvo la funcionalidad original, pero se reorganizó el código
    en nuevos archivos.
    
    Además, se actualizó el constructor de la clase `BadRequestException`
    para invocar al constructor base de `Exception` con el parámetro
    `message`, asegurando que el mensaje de error se propague
    correctamente.
    
    Estos cambios mejoran la claridad, consistencia y mantenibilidad
    del código.

---
+++ Examples.CleanArquitecture/Examples.CleanArquitecture.Application/Features/Person/Queries/GetPerson/GetPersonByDniQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Examples.CleanArquitecture.Application.Contracts.Persistence;
+using Examples.CleanArquitecture.Application.DTO;
+using Examples.CleanArquitecture.Application.Exceptions;
+using MediatR;
+
+namespace Examples.CleanArquitecture.Application.Features.Person.Queries.GetPerson;
+
+public class GetPersonByDniQueryHandler : IRequestHandler<GetPersonByDniQuery, PersonDto>
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    private readonly IPersonRepository _personRepository;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private readonly IMapper _mapper;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="leaveAllocationRepository"></param>
+    /// <param name="mapper"></param>
+    public GetPersonByDniQueryHandler(IPersonRepository leaveAllocationRepository,
+        IMapper mapper)
+    {
+        this._personRepository = leaveAllocationRepository;
+        this._mapper = mapper;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async Task<PersonDto> Handle(GetPersonByDniQuery request, CancellationToken cancellationToken = default)
+    {
+        var validator = new GetPersonQueryValidator(_personRepository);
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.Errors.Any())
+            throw new BadRequestException("Invalid DNI value", validationResult);
+
+        var person = await _personRepository.GetByDniAsync(request.dni);
+
+        return this._mapper.Map<PersonDto>(person);
+    }
+}
